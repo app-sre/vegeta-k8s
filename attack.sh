42d0c9c34590b8154ed238a5575aedf22193c28d
Rafa Porres Molina <rporresm@redhat.com>
2020-02-06 16:21:41 +0100

    Add name to attacks to fix vegeta plot issues
    see https://github.com/tsenart/vegeta/issues/333
    
    Signed-off-by: Rafa Porres Molina <rporresm@redhat.com>

--- attack.sh
+++ attack.sh
@@ -20,23 +20,23 @@ done
 
 [[ $count -gt 0 ]] && exit 1
 
-REPORT="$TEMP_DIR/vegeta-results-$ATTACK_NAME-$(date +%Y%m%d%H%M%S)-$ATTACK_POD.bin"
+VEGETA_RESULT="$TEMP_DIR/vegeta-results-$ATTACK_NAME-$(date +%Y%m%d%H%M%S)-$ATTACK_POD.bin"
 
 function log() {
     echo "$(date +%Y%m%d%H%M%S) - $1"
 }
 
 function copy_report() {
-    log "Copying $REPORT to $S3_BUCKET_NAME bucket"
-    aws s3 cp "$REPORT" "s3://$S3_BUCKET_NAME"
+    log "Copying $VEGETA_RESULT to $S3_BUCKET_NAME bucket"
+    aws s3 cp "$VEGETA_RESULT" "s3://$S3_BUCKET_NAME"
     local rc=$?
-    rm $REPORT
+    rm $VEGETA_RESULT
     exit $rc
 }
 
 trap copy_report INT TERM
 
 log "Running vegeta"
-echo "$COMMAND" | vegeta attack -rate="$RATE" -duration="$DURATION" > "$REPORT"
-
+echo "$COMMAND" | vegeta attack -name "$ATTACK_POD"  -rate="$RATE" -duration="$DURATION" > "$VEGETA_RESULT"
+vegeta report $VEGETA_RESULT
 copy_report
